59ab2a7b38670ba389214e1f3e73156344714517
Ratush Aleksandra <Ne0f1ex>
2021-03-14 20:53:30 +0400

    Fix lecturer remarks  - Change type of miles  - Code format

--- src/ru/netology/ratush/task21/Main.java
+++ src/ru/netology/ratush/task21/Main.java
@@ -2,13 +2,13 @@ package ru.netology.ratush.task21;
 
 public class Main {
 
-    public static final double RUBLES_FOR_ONE_MILE = 20.00;
+    public static int RUBLES_FOR_ONE_MILE = 20;
 
-    public static void main(String ... args) {
+    public static void main(String... args) {
 
         double ticketCost = 20000.12;
-        double miles = ticketCost / RUBLES_FOR_ONE_MILE;
-        System.out.println("Количество милей, начисленных за поездку "  + miles);
+        int miles = (int) (ticketCost / RUBLES_FOR_ONE_MILE);
+        System.out.println("Количество милей, начисленных за поездку " + miles);
 
     }
 
